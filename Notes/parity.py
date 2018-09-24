81ac078519e44efa8b4fec48008561e126e7a7f9
Malfred Koryor <34602737+shadowpy@users.noreply.github.com>
2018-09-24 11:54:16 -0500

    Update parity.py

--- Notes/parity.py
+++ Notes/parity.py
@@ -2,6 +2,10 @@
 # For example, the parity of 1011 is 1, and the parity of 10001000 is 0. Parity checks are used to detect single bit errors in data storage and communication. 
 # It is fairly straightforward to write code that computes the parity of a single 64-bit word.
 
+# The brute-force algorithm iteratively tests the value of each bit while tracking the number of ls seen so far. 
+# Since we only care if the number of ls is even or odd, we can store the number
+# mod 2.
+
 
 def parity(x):  
     result = 0
