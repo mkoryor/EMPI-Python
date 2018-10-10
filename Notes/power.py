15b152fc8c78a21c9ea5a0115f582db94992eda0
Malfred Koryor <34602737+shadowpy@users.noreply.github.com>
2018-10-09 23:01:20 -0500

    Create power.py

---
+++ Notes/power.py
@@ -0,0 +1,22 @@
+
+
+def power(x, y):
+    result, power = 1.0, y
+    if y < 0:
+        power, x = -power, 1.0 / x
+    while power:
+        if power & 1:
+            result *= x
+        x, power = x * x, power >> 1
+    return result 
+    
+
+#The number of multiplication is at most twice the index of y's MSB, implying an O(n) time complexity 
+
+
+
+
+
+
+
+
