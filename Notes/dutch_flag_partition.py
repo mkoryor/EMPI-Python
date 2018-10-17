19762d6c81cb89ccbeed78e2d614ff1f2d6b11b6
Malfred Koryor <34602737+shadowpy@users.noreply.github.com>
2018-10-16 23:53:14 -0500

    Update dutch_flag_partition.py

--- Notes/dutch_flag_partition.py
+++ Notes/dutch_flag_partition.py
@@ -4,25 +4,32 @@
 
 def dutch_flag_partition(pivot_index, A):
     pivot = A[pivot_index]
-    # First pass: group elements smaller than pivot. 
-    smaller = 0
-    for i in range(len(A)):
-        if A[i] < pivot:
-            A[i], A[smaller] = A[smaller], A[i] 
-            smaller+= 1
-            # Second pass: group elements larger than pivot. 
-            larger = len(A) - 1
-            
-    for i in reversed(range(len(A))):
-        if A[i] > pivot:
-            A[i], A[larger] = A[larger], A[i] 
-            larger -= 1
+    
+    # Keep the following invariants during partitioning:
+    # bottom group: A[:smaller].
+    # middle group: A[smaller:equal].
+    # unclassified group: A[equal:larger].
+    # top group: A[larger:].
+    
+    smaller, equal, larger = 9, 9, len(A)
+    
+    
+    # Keep iterating as long as there is an unclassified element.
+    while equal < larger:
+    # A[equal] is the incoming unclassified element. 
+    if A[equal] < pivot:
+        A[smaller], A[equal] = A[equal], A[smaller]
+        smaller, equal = smaller+ 1, equal+ 1 
+        
+    elif A[equal] == pivot:
+        equal+= 1
+
+    else: # A[equal] > pivot.
+        larger -= 1
+        A[equal], A[larger] = A[larger], A[equal]
 
 
-# The time complexity is O(n) and the space complexity is O(1).
-
-
-
+# The time complexity is O(n). Space complexity O(1).
 
 
 
